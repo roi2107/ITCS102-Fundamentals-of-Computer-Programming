3a1dbe080f060489d7e62021b8f8e9ba31bd9d01
roi2107 <johnroitesiorna11@gmail.com>
2026-08-26 11:24:41 +0800

    Activity 5

---
+++ acitivty5.py
@@ -0,0 +1,19 @@
+#operators activity
+ 
+a = 6
+b = 7
+sum = a + b
+diff = a-b
+multiplied = a*b
+division = a/b
+modulo = a%b
+exponent = a**b
+floor_division = a//b
+
+print("The sum of ",a," and ",b", is", sum)
+print("The diff of ",a," and ",b", is", diff)
+print("The multiplied of ",a," and ",b", is", multiplied)
+print("The division of ",a," and ",b", is", division)
+print("The modulo of ",a," and ",b", is", modulo)
+print("The exponent of ",a," to the power ",b", is", exponent)
+print("The floor division of ",a," and ",b", is", floor_division)
