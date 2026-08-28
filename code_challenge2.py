1676d752721dfc7f0aa6f3c59ba34e2396cbcad0
roi2107 <johnroitesiorna11@gmail.com>
2026-08-28 21:06:13 +0800

    code challenge 2

---
+++ code_challenge2.py
@@ -0,0 +1,35 @@
+# money deposited
+
+money = 19863
+
+print("the money deposited is:", money)
+
+# computation
+dot = money // 1000
+money = money % 1000
+dofh = money // 500
+money = money % 500
+doth = money // 200
+money = money % 200
+doh = money // 100
+money = money % 100
+dfv = money // 50
+money = money % 50
+dtw = money // 20
+money = money % 20
+dt = money // 10
+money = money % 10
+df = money // 5
+money = money % 5
+do = money // 1
+money = money % 1
+
+print(dot, "- 1000")
+print(dofh, "- 500")
+print(doth, "- 200")
+print(doh, "- 100")
+print(dfv, "- 50")
+print(dtw, "- 20")
+print(dt, "- 10")
+print(df, "- 5")
+print(do, "- 1")
