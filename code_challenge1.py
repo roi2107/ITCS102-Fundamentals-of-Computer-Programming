8823fde8dec1bd53d2e95b29fc56c114a64847db
roi2107 <johnroitesiorna11@gmail.com>
2026-08-28 22:33:38 +0800

    code challenge1

---
+++ code_challenge1.py
@@ -0,0 +1,4 @@
+a = "\t\t\t\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*"
+b = "\n\t\t\t\t\t\t\t\t\t\t**\t\t\t\t\t\t**\n\t\t\t\t\t\t\t\t\t\t**\t\t\t\t\t\t**\n\t\t\t\t\t\t\t\t\t\t**\t\t\t\t\t\t**\n\t\t\t\t\t\t\t\t\t\t**\t\tHi my name is Roi\t\t**\n\t\t\t\t\t\t\t\t\t\t**\t\tfrom BSIT-1B\t\t\t**\n\t\t\t\t\t\t\t\t\t\t**\t\t\t\t\t\t**\n\t\t\t\t\t\t\t\t\t\t**\t\t\t\t\t\t**"
+c = "\n\t\t\t\t\t\t\t\t\t\t**************************************************\n\t\t\t\t\t\t\t\t\t\t**************************************************"
+print(a, b, c)
