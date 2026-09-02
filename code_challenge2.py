3921cb6e18f2894bcaf91b1d2fca458e43c45150
roi2107 <johnroitesiorna11@gmail.com>
2026-09-02 21:38:41 +0800

    edited

--- code_challenge2.py
+++ code_challenge2.py
@@ -1,35 +1,32 @@
-# money deposited
+#code challenge 2
 
-money = 19863
+d = eval(input("Enter the amount of deposit: "))
 
-print("the money deposited is:", money)
+n1 = d // 1000
+d = d % 1000
+n2 = d // 500
+d = d % 500
+n3 = d // 200
+d = d % 200
+n4 = d // 100
+d = d % 100
+n5 = d // 50
+d = d % 50
+n6 = d // 20
+d = d % 20
+n7 = d // 10
+d = d % 10
+n8 = d // 5
+d = d % 5
+n9 = d // 1
+d = d % 1
 
-# computation
-dot = money // 1000
-money = money % 1000
-dofh = money // 500
-money = money % 500
-doth = money // 200
-money = money % 200
-doh = money // 100
-money = money % 100
-dfv = money // 50
-money = money % 50
-dtw = money // 20
-money = money % 20
-dt = money // 10
-money = money % 10
-df = money // 5
-money = money % 5
-do = money // 1
-money = money % 1
-
-print(dot, "- 1000")
-print(dofh, "- 500")
-print(doth, "- 200")
-print(doh, "- 100")
-print(dfv, "- 50")
-print(dtw, "- 20")
-print(dt, "- 10")
-print(df, "- 5")
-print(do, "- 1")
+print(n1, "- 1000")
+print(n2, "- 500")
+print(n3, "- 200")
+print(n4, "- 100")
+print(n5, "- 50")
+print(n6, "- 20")
+print(n7, "- 10")
+print(n8, "- 5")
+print(n9, "- 1")
