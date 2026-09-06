39b99a63185ab264519c34e16baf19f64f14bb84
roi2107 <johnroitesiorna11@gmail.com>
2026-09-07 06:13:42 +0800

    activity7.py

---
+++ Activity7.py
@@ -0,0 +1,33 @@
+total = 20
+ 
+r = eval(input("enter any number --> "))
+total = total + r
+
+o= eval(input("enter any number --> "))
+total += o
+
+i= eval(input("enter any number --> "))
+total *= i
+
+q= eval(input("enter any number --> "))
+total += q
+
+w= eval(input("enter any number --> "))
+total *= w
+
+e= eval(input("enter any number --> "))
+total -= e
+
+r= eval(input("enter any number --> "))
+total *= r
+
+t= eval(input("enter any number --> "))
+total -= t
+
+y= eval(input("enter any number --> "))
+total += y
+
+o= eval(input("enter any number --> "))
+total *= o
+
+print("Current Total is --> ", total)
