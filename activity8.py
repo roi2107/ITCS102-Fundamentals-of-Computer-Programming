5bbf8543e97adba3fff7201e7dabf808da2602df
roi2107 <johnroitesiorna11@gmail.com>
2026-09-07 06:20:46 +0800

    activity8.py

---
+++ activity8.py
@@ -0,0 +1,18 @@
+hobbies = "" #empty string
+
+r = input("What are your hobbies --> ")
+hobbies += r + ", "
+
+o = input("What are your hobbies --> ")
+hobbies += o + ", "
+
+i = input("What are your hobbies --> ")
+hobbies += i + ", "
+
+w = input("What are your hobbies --> ")
+hobbies += w + ", "
+
+p = input("What are your hobbies --> ")
+hobbies += p + ", "
+
+print("My hobbies are {", hobbies , "}")
