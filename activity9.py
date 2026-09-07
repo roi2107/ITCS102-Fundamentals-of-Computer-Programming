cdcc574538e5fd0266314eb746132918408103e4
roi2107 <johnroitesiorna11@gmail.com>
2026-09-07 10:56:41 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,20 @@
+x = 50
+y= 20
+
+print( y > x)
+print(y<x)
+print(x!=y)
+print(x<=y)
+print(x>=y)
+print(x==y)
+
+name1 = "roi"
+name2 = "yeti"
+
+print(name1<name2)
+print(name1>name2)
+print(name1<=name2)
+print(name1>=name2)
+print(name1!=name2)
+print(name1==name2)
+
