ef919c4e485977b436010ddc3daaec52360b3314
roi2107 <johnroitesiorna11@gmail.com>
2026-09-07 11:14:08 +0800

    activity10

---
+++ activity10.py
@@ -0,0 +1,11 @@
+username = "roiroi123"
+password = "kumag123"
+
+u = input("Input Username ---> ")
+p = input("Input Password ---> ")
+
+if u == username and p == password: 
+     print("username and password correct")
+
+else:
+     print("ACCESS DENIED")
