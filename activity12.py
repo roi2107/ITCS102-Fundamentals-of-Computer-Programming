549fcb00ba5382d1485bfc935cdb859dca679b94
roi2107 <johnroitesiorna11@gmail.com>
2026-09-09 11:01:55 +0800

    activity12

---
+++ activity12.py
@@ -0,0 +1,90 @@
+name = input("Please input you name ---> ")
+
+age = int(input("Please input your age ---> "))
+
+if age >= 0 and age <=5 :
+        print("That age is considered as INFANT")
+
+elif age >= 0 and age <=5 :
+        print("That age is considered as INFANT")
+
+else:
+        print("age is invalid")
+
+name = input("Please input you name ---> ")
+
+age = int(input("Please input your age ---> "))
+
+if age >= 6 and age <=12 :
+        print("That age is considered as KID")
+
+elif age >= 6 and age <=12 :
+        print("That age is considered as KID")
+
+else:
+        print("age is invalid")
+
+name = input("Please input you name ---> ")
+
+age = int(input("Please input your age ---> "))
+
+if age >= 13 and age <=15 :
+        print("That age is considered as PRE TEEN")
+
+elif age >= 13 and age <=15 :
+        print("That age is considered as PRE TEEN")
+
+else:
+        print("age is invalid")
+
+name = input("Please input you name ---> ")
+
+age = int(input("Please input your age ---> "))
+
+if age >= 16 and age <=19 :
+        print("That age is considered as TEENAGER")
+
+elif age >= 16 and age <=19 :
+        print("That age is considered as TEENAGER")
+
+else:
+        print("age is invalid")
+
+name = input("Please input you name ---> ")
+
+age = int(input("Please input your age ---> "))
+
+if age >= 20 and age <=29 :
+        print("That age is considered as EARLY ADULT")
+
+elif age >= 20 and age <=29 :
+        print("That age is considered as EARLY ADULT")
+
+else:
+        print("age is invalid")
+
+name = input("Please input you name ---> ")
+
+age = int(input("Please input your age ---> "))
+
+if age >= 30 and age <=58 :
+        print("That age is considered as ADULT")
+
+elif age >= 30 and age <=58 :
+        print("That age is considered as ADULT")
+
+else:
+        print("age is invalid")
+
+name = input("Please input you name ---> ")
+
+age = int(input("Please input your age ---> "))
+
+if age >= 59 and age <=150 :
+        print("That age is considered as SENIOR")
+
+elif age >= 59 and age <=150 :
+        print("That age is considered as SENIOR")
+
+else:
+        print("age is invalid")
